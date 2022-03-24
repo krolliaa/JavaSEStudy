15d0881f6e27f3c740a29deeb7a7342f49a54a04
krolliaa <krolliaa@163.com>
2022-03-24 15:05:39 +0800

    this 关键字

---
+++ src/Chapter02/AboutThis.java
@@ -0,0 +1,8 @@
+package Chapter02;
+
+public class AboutThis {
+    /*
+        什么是 this 关键字？其实就是对象自己，this 存放在堆内存对象本身空间当中，存放的地址是对象所在的地址。
+        也就是说先有了对象，才有了 this，没有对象就没有 this 这个说法。this指向的是对象本身。
+    */
+}
