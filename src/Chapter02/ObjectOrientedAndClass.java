a345be4ac566b9639a062a2c8a6b38aa7cfb1ebe
krolliaa <krolliaa@163.com>
2022-03-24 14:21:43 +0800

    面向对象和类

---
+++ src/Chapter02/ObjectOrientedAndClass.java
@@ -0,0 +1,9 @@
+package Chapter02;
+
+public class ObjectOrientedAndClass {
+    /*
+        面向对象(Object Oriented)三大特征：封装、继承、多态
+        类是对象的模板，对象就是通过模板实例化得来的，类是一个个对象的抽象，抽取出这些个对象的共同特征就形成了类
+        类=属性+方法。属性其实就是实例变量 ---> 先有对象才可以访问到实例变量。
+    */
+}
