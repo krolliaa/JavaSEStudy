411d8ca03dbd0eb9c3d7656028889ef0de104209
krolliaa <krolliaa@163.com>
2022-03-25 11:30:52 +0800

    访问权限修饰符

---
+++ src/Chapter02/AccessStudy.java
@@ -0,0 +1,11 @@
+package Chapter02;
+
+public class AccessStudy {
+    /*
+        Java访问权限修饰符包括：private default protected public
+        private：只有当前类的成员可以访问
+        default：只有当前类和在同个包的成员可以访问[默认]
+        protected：当前类、同个包的其它类、子类可以访问
+        public：公共的，所有类都可以访问
+    */
+}
